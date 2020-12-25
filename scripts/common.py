df7269d4032f9bb114199785e39879cfb7ca5168
David Cabrera <dacabdi@gmail.com>
2020-12-25 17:05:29 -0500

    add serial buffer size override pre action script

---
+++ scripts/common.py
@@ -0,0 +1,8 @@
+import fileinput
+import sys
+
+def replaceAll(file, searchExp, replaceExp):
+    for line in fileinput.input(file, inplace=1):
+        if searchExp in line:
+            line = line.replace(searchExp,replaceExp)
+        sys.stdout.write(line)
