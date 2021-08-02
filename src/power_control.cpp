0f050904eee19e20c53c391b7d2ce24cf83c3e66
Julio J. Cabrera <jjcabreraterainsights@gmail.com>
2021-08-02 02:05:20 -0400

    added emergency poweroff

--- src/power_control.cpp
+++ src/power_control.cpp
@@ -10,6 +10,7 @@ namespace GNSS_RTK_ROVER
     int CPUPowerController::chargingStatePin;
     bool CPUPowerController::chargingState;
     bool CPUPowerController::justChangedOnOff;
+    uint8_t CPUPowerController::epoweroffCount;
     uint64_t CPUPowerController::powerSwitchLastPressed;
     std::function<void(bool)> CPUPowerController::onTurnOnOff;
     std::function<void(bool)> CPUPowerController::onChargingChanged;
@@ -28,6 +29,7 @@ namespace GNSS_RTK_ROVER
         onOffState = false;
         chargingState = false;
         onTurnOnOff = _onTurnOnOff;
+        epoweroffCount = 0;
         onChargingChanged = _onChargingChanged;
         justChangedOnOff = false;
         attachInterrupt(digitalPinToInterrupt(onOffPin), onOffSwitcher, FALLING);
@@ -43,6 +45,18 @@ namespace GNSS_RTK_ROVER
         auto now = millis();
         if(now - powerSwitchLastPressed <= 100) // Debouncer
             return;
+
+        if(now - powerSwitchLastPressed <= 1000)
+        {
+            epoweroffCount++;
+        } else {
+            epoweroffCount = 0;
+        }
+        if(epoweroffCount >= 5)
+        {
+            Serial.println("Powering Off Forcefully");
+            turnOffPowerModule();
+        }
 
         Serial.println("OnOff Switch Pressed");
         if(onOffState)
